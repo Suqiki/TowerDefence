a366cd10aeefb13443a9171291237b6cb8d72515
Suqiki <raresiovanel16@gmail.com>
2026-02-26 12:44:56 +0200

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        if (Waypoints.points == null || Waypoints.points.Length == 0)
+        {
+            Debug.LogError("Waypoints not set!");
+            return;
+        }
+
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        
+        if (Vector3.Distance(transform.position, target.position) < 0.25f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
